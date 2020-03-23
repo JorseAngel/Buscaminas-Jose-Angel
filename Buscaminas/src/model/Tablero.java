7ac5738edd22c3d9e98e143ace333aacc2671841
JorseAngel <49229953+JorseAngel@users.noreply.github.com>
2020-03-23 11:56:06 +0100

    Primera version Modelo_Jose

---
+++ Buscaminas/src/model/Tablero.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Tablero {
+
+	private Casilla[][] casillas;
+
+	public Tablero(int lado, int numeroBombas) {
+		super();
+		crearTablero(lado);
+		colocarMinas(lado,numeroBombas);
+		establecerMinasAlrededor();
+	}
+	
+	private void establecerMinasAlrededor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void colocarMinas(int lado, int numeroBombas) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void crearTablero(int lado) {
+		this.casillas=new Casilla[lado][lado];
+		// ahora hay que crear los objetos casilla
+	}
+
+	//TODO antes era private
+	public Casilla getCasilla(Coordenada posicion) {
+		return casillas[posicion.getPosX()][posicion.getPosY()];
+	}
+	
+	private void setMina(Coordenada posicion, boolean bandera) {
+		getCasilla(posicion).setMina(bandera);
+	}
+
+	private boolean isMina(Coordenada posicion) {
+		return getCasilla(posicion).isMina();
+	}
+
+	
+	
+}
