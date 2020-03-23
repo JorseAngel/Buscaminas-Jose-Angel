10852a1d8c1ef987a954c8a97aa92e50404d1d02
JorseAngel <49229953+JorseAngel@users.noreply.github.com>
2020-03-23 13:57:51 +0100

    Colocar mina REALIZADO

--- Buscaminas/src/model/Tablero.java
+++ Buscaminas/src/model/Tablero.java
@@ -1,5 +1,7 @@
 package model;
 
+import utiles.Utiles;
+
 public class Tablero {
 
 	private Casilla[][] casillas;
@@ -7,30 +9,72 @@ public class Tablero {
 	public Tablero(int lado, int numeroBombas) {
 		super();
 		crearTablero(lado);
-		colocarMinas(lado,numeroBombas);
+		colocarMinas(lado, numeroBombas);
 		establecerMinasAlrededor();
 	}
-	
+
 	private void establecerMinasAlrededor() {
-		// TODO Auto-generated method stub
-		
+		// TODO
+
 	}
 
 	private void colocarMinas(int lado, int numeroBombas) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private void crearTablero(int lado) {
-		this.casillas=new Casilla[lado][lado];
-		// ahora hay que crear los objetos casilla
+		int tamano = 2;
+		int posicionesAleatorias[][] = new int[numeroBombas][tamano];
+		rellenarPosicionesAleatorias(lado, posicionesAleatorias);
+
+		for (int i = 0; i < posicionesAleatorias.length; i++) {
+			for (int j = 0; j < posicionesAleatorias[i].length - 1; j++) {
+				Coordenada miCoordenada = new Coordenada(posicionesAleatorias[i][j], posicionesAleatorias[i][j + 1]);
+
+				if (getCasilla(miCoordenada).isMina()) {
+					do {
+						otraPosicionAleatoria(lado, posicionesAleatorias, i);
+						miCoordenada = new Coordenada(posicionesAleatorias[i][j], posicionesAleatorias[i][j + 1]);
+					} while (getCasilla(miCoordenada).isMina());
+				}
+				getCasilla(miCoordenada).setMina(true);
+			}
+		}
+
 	}
 
-	//TODO antes era private
+	private void otraPosicionAleatoria(int lado, int[][] posicionesAleatorias, int posicion) {
+		int longitud = 2;
+		for (int i = 0; i < longitud; i++) {
+			posicionesAleatorias[posicion][i] = Utiles.dameNumero(lado);
+		}
+	}
+
+	private void rellenarPosicionesAleatorias(int lado, int[][] posicionesAleatorias) {
+		int aletorio = 0;
+
+		for (int i = 0; i < posicionesAleatorias.length; i++) {
+			for (int j = 0; j < posicionesAleatorias[i].length; j++) {
+				posicionesAleatorias[i][j] = Utiles.dameNumero(lado);
+			}
+		}
+	}
+
+	private void crearTablero(int lado) {
+		this.casillas = new Casilla[lado][lado];
+
+		for (int i = 0; i < casillas.length; i++) {
+			for (int j = 0; j < casillas.length; j++) {
+				this.casillas[i][j] = new Casilla();
+			}
+		}
+	}
+
+	public Casilla[][] getCasillas() {
+		return casillas;
+	}
+
+	// TODO antes era private
 	public Casilla getCasilla(Coordenada posicion) {
 		return casillas[posicion.getPosX()][posicion.getPosY()];
 	}
-	
+
 	private void setMina(Coordenada posicion, boolean bandera) {
 		getCasilla(posicion).setMina(bandera);
 	}
@@ -39,6 +83,4 @@ public class Tablero {
 		return getCasilla(posicion).isMina();
 	}
 
-	
-	
 }
